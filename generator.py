5be99d6916699bd6a8a5a121d42aa264c437c6f7
Milhone <jmilhone@wisc.edu>
2019-10-20 15:13:07 -0500

    Change upsampling to UpSample2D and Conv2d from Conv2DTranspose
    I was upsampling the image by using Conv2DTranpose but this leads to
    checkerboarding from what I have read online. Upsampling by nearest
    neighbor and applying a 2D convolution is supposed to eliminate the
    checkerboarding effect.

--- generator.py
+++ generator.py
@@ -13,35 +13,31 @@ def make_generator_model():
     model.add(layers.Reshape((4, 4, 1024)))
     assert model.output_shape == (None, 4, 4, 1024)
 
-    #Upscale using Conv2DTranspose
-    # ToDo: Look into upsampling via interpolation and 2d Convolution
-    model.add(layers.Conv2DTranspose(512, (5, 5), strides=(2, 2), padding='same',
-                                     use_bias=False,
-                                     kernel_initializer=weight_initializer))
+    #Upscale using Upsampling2D and Convolution2D
+    model.add(layers.UpSampling2D(size=(2, 2), interpolation='nearest'))
+    model.add(layers.Conv2D(512, (5, 5), padding='same', use_bias=False,
+                            kernel_initializer=weight_initializer))
     assert model.output_shape == (None, 8, 8, 512)
-    # model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.BatchNormalization())
 
-    model.add(layers.Conv2DTranspose(256, (5, 5), strides=(2, 2), padding='same',
-                                     use_bias=False,
-                                     kernel_initializer=weight_initializer))
+    model.add(layers.UpSampling2D(size=(2, 2), interpolation='nearest'))
+    model.add(layers.Conv2D(256, (5, 5), padding='same', use_bias=False,
+                            kernel_initializer=weight_initializer))
     assert model.output_shape == (None, 16, 16, 256)
-    # model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.BatchNormalization())
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same',
-                                     use_bias=False,
-                                     kernel_initializer=weight_initializer))
+    model.add(layers.UpSampling2D(size=(2, 2), interpolation='nearest'))
+    model.add(layers.Conv2D(128, (5, 5), padding='same', use_bias=False,
+                            kernel_initializer=weight_initializer))
     assert model.output_shape == (None, 32, 32, 128)
-    # model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.BatchNormalization())
 
-    model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same',
-                                     use_bias=False, activation='tanh',
-                                     kernel_initializer=weight_initializer))
+    model.add(layers.UpSampling2D(size=(2, 2), interpolation='nearest'))
+    model.add(layers.Conv2D(3, (5, 5), padding='same', use_bias=False,
+                            kernel_initializer=weight_initializer, activation='tanh'))
     assert model.output_shape == (None, 64, 64, 3)
     return model
 
